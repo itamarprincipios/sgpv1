b2e5ea38bf17ca29d5feb5560c6fa3097cf541ef
itamarprincipios <itamarprincipios@gmail.com>
2026-01-07 17:56:08 -0400

    Fix require path in create_missing_tables.php

--- public/create_missing_tables.php
+++ public/create_missing_tables.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/app/Core/Database.php';
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../app/Core/Database.php';
 
 try {
     $db = Database::getInstance()->getConnection();
